c2bd2a1eddc437af565ef14fcaa71182c13db50b
Peter Csikos <pcsikos@gmail.com>
2021-10-19 16:47:54 +0200

    Installed unimodules

--- App.js
+++ App.js
@@ -7,11 +7,7 @@
  */
 
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
